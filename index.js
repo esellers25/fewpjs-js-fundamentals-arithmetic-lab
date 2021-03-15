9450acedda80c8c40263c3c35f670d87f5d287cc
esellers25 <erin.e.sellers@gmail.com>
2021-03-15 17:29:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2
+const random = Math.floor(Math.random()* 100) + 1;
+let num3 = 25
+let num4 = 7
+const mod = num3 % num4
+const max = Math.max(3, 16, 5, 20)
